8dc3aebd791c5497058d6559e5ae692e27fe159c
Johann Schopplich <mail@johannschopplich.com>
2023-06-22 20:13:54 +0200

    chore: release v4.0.4

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInit87ac086e0582aff779837a1930970fff
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit87ac086e0582aff779837a1930970fff::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
