fb072e103d512d6440a205f2a7e83a6e31e0473e
Johann Schopplich <mail@johannschopplich.com>
2023-07-07 16:39:23 +0200

    chore: release v4.1.0

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'johannschopplich/kirby-helpers',
-        'pretty_version' => '4.0.4',
-        'version' => '4.0.4.0',
+        'pretty_version' => '4.1.0',
+        'version' => '4.1.0.0',
         'reference' => NULL,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -29,8 +29,8 @@
             'dev_requirement' => false,
         ),
         'johannschopplich/kirby-helpers' => array(
-            'pretty_version' => '4.0.4',
-            'version' => '4.0.4.0',
+            'pretty_version' => '4.1.0',
+            'version' => '4.1.0.0',
             'reference' => NULL,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
