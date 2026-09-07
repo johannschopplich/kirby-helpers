f42f27a56277a0a251974fe9edfe6eadd0e9dbeb
Johann Schopplich <johann@schopplich.com>
2026-09-07 09:59:31 +0200

    chore: ship the vendor directory with each release

--- scripts/pre-release.mjs
+++ scripts/pre-release.mjs
@@ -5,4 +5,7 @@ const composer = await fs.readJson("./composer.json");
 composer.version = version;
 await fs.writeJson("./composer.json", composer, { spaces: 2 });
 await $`composer update`;
+await fs.remove("./vendor");
 await $`composer dist`;
+await $`find vendor/composer -maxdepth 1 -type f -name 'tmp-*' -delete`;
+await $`git add -f vendor/`;
